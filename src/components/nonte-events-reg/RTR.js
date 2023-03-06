872e64c4782d4cdd1f12d594bde2574789ee2c0f
HarikowshikMolugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-03-06 14:14:16 +0530

    Update RTR.js

--- src/components/nonte-events-reg/RTR.js
+++ src/components/nonte-events-reg/RTR.js
@@ -29,14 +29,14 @@ const RTR = () => {
          <p><b style={{color:"#A4C636"}}>Venue</b> : <b id="r-venue"style={{color:"whitesmoke",fontWeight:"500"}}>AB-II BLOCK</b></p><br/>
          
          <p><b style={{color:"#A4C636"}}>Event Registration Fee (per person) :</b>  <b style={{color:"whitesmoke",fontWeight:"500"}}>70/-</b></p><br/>
-         <p><b style={{color:"#A4C636"}}>Prize money :</b>  <b style={{color:"whitesmoke",fontWeight:"500"}}>200 Rs + APPRECIATION CERTIFICATE</b></p><br/>
+         <p><b style={{color:"#A4C636"}}>Prize money :</b>  <b style={{color:"whitesmoke",fontWeight:"500"}}>500 Rs + APPRECIATION CERTIFICATE + Exciting gifts</b></p><br/>
          <p><b style={{color:"#A4C636"}}>PAY USING MOBILE NUMBER :</b><b style={{color:"whitesmoke",fontWeight:"500"}}>9985228800</b></p><br/>
          <p><b style={{color:"#A4C636"}}>PAY WITH QR CODE :</b></p>
          <div className='qr-div'><img src='/images/QRCode_70Rs.jpg' className='qr'></img></div>
          <div id='reg'><a id='reg-btn' href='https://docs.google.com/forms/d/e/1FAIpQLSdYlDVQiL-qL_tm88X85sRmDDOPjsYo0hD4CT8qqeh6vMz4Dg/viewform?usp=sf_link'>Register</a></div>
       </div>
       <div id='poster'>
-        <img src="/images/RTRL.png"></img>
+        <img src="/images/RTRL.jpg"></img>
         </div>
     </div>
   </div>
