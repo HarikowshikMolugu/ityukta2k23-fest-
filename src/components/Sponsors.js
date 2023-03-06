a28e83059f7d72d7d7dfdab417e780ef18efbad2
HarikowshikMolugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-03-06 10:10:44 +0530

    Add files via upload

---
+++ src/components/Sponsors.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FOOTER from './FOOTER'
+const Sponsors = () => {
+  return (
+    <div>
+    <div className='sponsers'>
+         <p style={{color:"#FF9900",fontSize:"30px"}}>Updating...</p>
+       </div>
+       <FOOTER></FOOTER>
+       </div>
+  )
+}
+
+export default Sponsors
