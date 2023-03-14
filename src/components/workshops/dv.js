b81f76e54e6434dbeef8c7465cf3ed9ecce6e6d2
HarikowshikMolugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-03-14 11:07:06 +0530

    Update dv.js

--- src/components/workshops/dv.js
+++ src/components/workshops/dv.js
@@ -40,7 +40,7 @@ Together, data analytics and visualization can help organizations and individual
          <p><b style={{color:"#A4C636"}}>PAY USING MOBILE NUMBER :</b><b style={{color:"whitesmoke",fontWeight:"500"}}>9985228800</b></p><br/>
          <p><b style={{color:"#A4C636"}}>PAY WITH QR CODE :</b></p>
          <div className='qr-div'><img src='/images/QRCode_800Rs.jpg' className='qr'></img></div>
-         <p><b style={{color:"red"}}>NOTE* :</b>  <b style={{color:"whitesmoke",fontWeight:"500"}}>For better experience carry your own laptops.</b></p><br/>
+         <p><b style={{color:"red"}}>NOTE* :</b><br/><br/>  <b style={{color:"whitesmoke",fontWeight:"500"}}>1.Top 15 participants from the Workshop will be provided with a 2 months free summer internship.<br/><br/>2.For better experience carry your own laptops.</b></p><br/>
          <div id='reg'><a id='reg-btn' href='https://docs.google.com/forms/d/e/1FAIpQLSeOxSPdLy6UpDJd5Hh8D94ynBFTVlHfyojPdz_fh1feG3qQTw/viewform?usp=sf_link'>Register</a></div>
          
         </div>
