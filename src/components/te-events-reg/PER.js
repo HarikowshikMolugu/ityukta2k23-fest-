c1d35cd32e409a13a7610319dbd1548303a449ac
HarikowshikMolugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-03-14 15:30:01 +0530

    Update PER.js

--- src/components/te-events-reg/PER.js
+++ src/components/te-events-reg/PER.js
@@ -31,8 +31,8 @@ const PER = () => {
          
          <p><b style={{color:"#A4C636"}}>Event Time</b> : <b style={{color:"whitesmoke",fontWeight:"500"}}></b></p><br/>
          <p><b style={{color:"#A4C636"}}>Venue</b> : <b id="r-venue"style={{color:"whitesmoke",fontWeight:"500"}}>AB-II BLOCK- 2ND FLOOR- COMPUTER LAB</b></p><br/>
-         <p><b style={{color:"#A4C636"}}>Event Registration Fee (per person) :</b>  <b style={{color:"whitesmoke",fontWeight:"500"}}>150/-</b></p><br/>
-         <p><b style={{color:"#A4C636"}}>Winner gets :</b>  <b style={{color:"whitesmoke",fontWeight:"500"}}>APPRECIATION CERTIFICATE + 300/- Prize money</b></p><br/>
+         <p><b style={{color:"#A4C636"}}>Event Registration Fee (per person) :</b>  <b style={{color:"whitesmoke",fontWeight:"500"}}>200/-</b></p><br/>
+         <p><b style={{color:"#A4C636"}}>Winner gets :</b>  <b style={{color:"whitesmoke",fontWeight:"500"}}>APPRECIATION CERTIFICATE + Exciting prize</b></p><br/>
          
          <p><b style={{color:"#A4C636"}}>Others gets :</b>  <b style={{color:"whitesmoke",fontWeight:"500"}}>PARTICIPATION CERTIFICATE</b></p><br/>
          
@@ -40,7 +40,7 @@ const PER = () => {
          <div className='qr-div' >
          <p><b style={{color:"#A4C636"}}>PAY USING MOBILE NUMBER :</b><b style={{color:"whitesmoke",fontWeight:"500"}}>9985228800</b></p><br/>
          <p><b style={{color:"#A4C636"}}>PAY WITH QR CODE :</b></p><br/>
-          <img src='/images/QRCode_150Rs.jpg' className='qr'></img></div>
+          <img src='/images/QRCode_200Rs.jpg' className='qr'></img></div>
          </div>
          <div id='reg'><a id='reg-btn' href='https://docs.google.com/forms/d/e/1FAIpQLSfwFJ-mvJNqY8SltKhcACY5bKbw3beWhPF82pKVtOkk0WRJpg/viewform?usp=sf_link'>Register</a></div>
         </div>
