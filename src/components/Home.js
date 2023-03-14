1d7835c7cd25b73b0ecaf971d11af35ef27dc6f6
HarikowshikMolugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-03-14 14:42:36 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -100,7 +100,7 @@ function Home() {
        <div id="e-head0">
           <div id="cg1"  >
             <div id="cg1-logo">
-               <img src={cg1}></img>
+               <img src="/images/cg1.jpg"></img>
             </div>
             <div id="c-p-content" style={{borderLeft:"3px solid #FF9900"}}>
             <p id="c-p-h"><u>CHEIFGUEST</u></p>
@@ -113,7 +113,7 @@ function Home() {
           </div>
           <div id="cg2"   >
           <div id="cg2-logo">
-          <img src={cg2}></img>
+          <img src="/images/cg2.jpg"></img>
           </div>
           <div id="fa-co-content" style={{borderLeft:"3px solid #FF9900"}}>
           <p id="fa-co-h"><u>CHEIFGUEST</u></p>
