5be1151031cb6d715d0bc055186366d7c5feb1ea
HarikowshikMolugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-03-15 17:23:58 +0530

    Update Sponsors.js

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -2,9 +2,18 @@ import React from 'react'
 import FOOTER from './FOOTER'
 const Sponsors = () => {
   return (
-    <div>
-    <div className='sponsers'>
-         <p style={{color:"#FF9900",fontSize:"30px"}}>Updating...</p>
+   <div className='sp-main'>
+
+    <div className='sp-total' >
+       <p id='sp-head'>OUR SPONSORS</p>
+      
+       <div className='sponsers2'>
+        <marquee className="ph-mar" direction="up">
+          <img id='spons2' src="/images/tm1.jpg"></img>
+          <img id='spons2' src="/images/tm2.jpg"></img>
+          <img id='spons2' src="/images/tm3.jpg"></img>
+        </marquee>
+       </div>
        </div>
        <FOOTER></FOOTER>
        </div>
