751d65d5e3a6f28e0afdbeaed94672a6159efbf3
HarikowshikMolugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-03-15 23:08:47 +0530

    Update Sponsors.js

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -9,10 +9,15 @@ const Sponsors = () => {
        <p id='sp-head'>OUR SPONSORS</p>
       
        <div className='sponsers2'>
-        <marquee className="ph-mar" direction="up" scrollamount="10">
+        <marquee className="ph-mar" direction="up" scrollamount="15">
           <img id='spons2' src="/images/tm1.jpg"></img>
           <img id='spons2' src="/images/tm2.jpg"></img>
           <img id='spons2' src="/images/tm3.jpg"></img>
+          <img id='spons2' src="/images/kvivek.jpg"></img>
+          <img id='spons2' src="/images/foc.jpg"></img>
+          <img id='spons2' src="/images/gau.jpg"></img>
+          <img id='spons2' src="/images/sr.jpg"></img>
+          <img id='spons2' src="/images/ysr.jpg"></img>
         </marquee>
        </div>
        </div>
